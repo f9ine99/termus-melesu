f1278cdc86e310735e270f811fd3c56c7e233ac3
Firaol <f9ine99@gmail.com>
2026-01-25 14:23:16 +0300

    fixing flash of unstyled content while refreshing the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  var colorTheme = localStorage.getItem('color-theme');
+                  if (colorTheme) {
+                    document.documentElement.setAttribute('data-theme', colorTheme);
+                  }
+                } catch (e) {}
+              })();
+            `,
+          }}
+        />
+      </head>
       <body className={`font-sans antialiased`}>
         <ThemeProvider
           attribute="class"

711345cfdb38d096bb19a0033622c6e03af9649f
Firaol <f9ine99@gmail.com>
2026-01-27 11:43:01 +0300

    configuration link for term and privacy

---
+++ lib/config.ts
@@ -0,0 +1,4 @@
+export const LEGAL_CONFIG = {
+    TERMS_OF_SERVICE_URL: process.env.NEXT_PUBLIC_TERMS_URL || "https://termus-melesu.vercel.app/terms",
+    PRIVACY_POLICY_URL: process.env.NEXT_PUBLIC_PRIVACY_URL || "https://termus-melesu.vercel.app/privacy",
+};
